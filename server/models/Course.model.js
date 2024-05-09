9421fcacad6455a8594754562c921e10926d3b78
SitoDP <jesusdprogram@gmail.com>
2024-05-09 12:57:38 +0200

    Course model and route

---
+++ server/models/Course.model.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const courseSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: true
+        },
+        description: {
+            type: String,
+            required: true
+        },
+        price: {
+            type: Number,
+            required: true
+        },
+        duration: {
+            type: Number,
+            required: true
+        },
+
+        image: {
+            type: String,
+        },
+        // owner: {
+        //     type: Schema.Types.ObjectId,
+        //     ref: 'User'
+        // },
+    },
+    {
+        timestamps: true,
+    }
+)
+
+
+const Course = mongoose.model('Course', courseSchema);
+
+module.exports = Course;
---
+++ server/routes/course.routes.js
@@ -0,0 +1,85 @@
+const express = require('express');
+const router = express.Router();
+const Course = require('../models/Course.model');
+const mongoose = require('mongoose');
+
+//Status 200: OK
+//Status 201: Created
+//Status 400: Bad Request
+//Status 404: Not Found
+//Status 500: Internal Server Error
+
+//Aqui me da el status 200 y me muestra todos los cursos
+
+
+
+// Obtener todos los cursos
+router.get('/', async (req, res) => {
+    try {
+        const courses = await Course.find();
+        return res.status(200).json(courses);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+// Obtener un curso por ID
+router.get('/:course_id', async (req, res) => {
+    try {
+        const course = await Course.findById(req.params.course_id);
+        if (!course) {
+            return res.status(404).json({ message: 'Course not found' });
+        }
+        return res.status(200).json(course);
+    } catch (err) {
+        return res.status(500).json({ message: err.message });
+    }
+});
+
+// Crear un nuevo curso
+router.post('/', async (req, res) => {
+    const course = {
+        ...req.body,
+    }
+
+    try {
+        const newCourse = await Course.create(course);
+        res.status(201).json(newCourse);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+// Eliminar un curso
+router.delete('/:course_id', async (req, res) => {
+    try {
+        const course = await Course.findById(req.params.id);
+        if (!course) {
+            return res.status(404).json({ message: 'Course not found' });
+        }
+
+        await course.remove();
+        res.json({ message: 'Course deleted' });
+    } catch (err) {
+        return res.status(500).json({ message: err.message });
+    }
+});
+
+// Actualizar un curso
+
+router.patch('/:course_id', async (req, res) => {
+    try {
+        const course = await Course.findById(req.params.course_id);
+        if (!course) {
+            return res.status(404).json({ message: 'Course not found' });
+        }
+
+        const updatedCourse = await Course.findByIdAndUpdate(req.params.course_id, req.body, { new: true });
+        res.json(updatedCourse);
+        return res.status(200).json(updatedCourse);
+    } catch (err) {
+        return res.status(500).json({ message: err.message });
+    }
+});
+
+module.exports = router;
