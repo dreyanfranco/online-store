f41c55eb4eb54989cd64c10e5faae8dc0ed32fa1
SofRiz <166594551+SofRiz@users.noreply.github.com>
2024-05-20 09:49:52 +0200

    categorias navbar

--- client/src/components/Carousel/PhotoSlider.jsx
+++ client/src/components/Carousel/PhotoSlider.jsx
@@ -1,75 +1,78 @@
-import { Carousel } from "react-bootstrap";
+import { Carousel, Container } from "react-bootstrap";
 import Photo1 from "../../components/Carousel/Images/Photo1.png";
 import Photo2 from "../../components/Carousel/Images/Photo2.png";
 import Photo3 from "../../components/Carousel/Images/Photo3.png";
 import Photo4 from "../../components/Carousel/Images/Photo4.png";
 import Photo5 from "../../components/Carousel/Images/Photo5.png";
 import Fondo from "../../components/Carousel/Images/Fondo.jpg";
+import "./Carousel.css"
 
 function PhotoSlider() {
   return (
-    <div className="col-12 bg-img" style={{ position: "relative" }}>
-      <img
-        src={Fondo}
-        style={{
-          position: "absolute",
-          top: 0,
-          left: 0,
-          width: "100%",
-          height: "100%",
-          objectFit: "cover",
-          opacity: 0.9,
-          filter: "grayscale(70%)"
-        
-        }}
-      />
-      <div
-        className="container d-flex justify-content-center align-items-center "
-        style={{ maxWidth: "50%" , padding:"50px 0"}}>
-        <Carousel className="">
+
+    <div className="p-3" style={{backgroundColor:"#b6e1f9"}}>
+
+    <div style={{ maxWidth: '50rem', margin: '0 auto' }}>
+      <Carousel>
           <Carousel.Item>
-            <img
-              className="img-fluid"
+          <img 
+              className="d-block w-100"
               src={Photo1}
-              alt="Image 1"
-              style={{ objectFit: "cover", width: "100%" }}
-            />
+            alt="Image 1"
+         
+             />
           </Carousel.Item>
           <Carousel.Item>
+         
             <img
-              className=" img-fluid "
+            
+              className="d-block w-100"
               src={Photo2}
               alt="Image 2"
-              style={{ objectFit: "cover", width: "100%" }}
+             /* style={{ objectFit: "cover", width: "100%" }}*/
             />
+             <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+              </Carousel.Caption>
+          
           </Carousel.Item>
           <Carousel.Item>
             <img
-              className="img-fluid "
+             
+              className="d-block w-100"
               src={Photo3}
               alt="Image 3"
-              style={{ objectFit: "cover", width: "100%" }}
+            /*  style={{ objectFit: "cover", width: "100%" }}*/
             />
+              <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
           </Carousel.Item>
           <Carousel.Item>
             <img
-              className="img-fluid "
+            
+              className="d-block w-100"
               src={Photo4}
               alt="Image 4"
-              style={{ objectFit: "cover", width: "100%" }}
+            /*  style={{ objectFit: "cover", width: "100%" }}*/
             />
           </Carousel.Item>
           <Carousel.Item>
             <img
-              className="img-fluid"
+          
+              className="d-block w-100"
               src={Photo5}
               alt="Image 5"
-              style={{ objectFit: "cover", width: "100%" }}
+            /*  style={{ objectFit: "cover", width: "100%" }}*/
             />
           </Carousel.Item>
-        </Carousel>
-      </div>
+      </Carousel>
+       </div>
+  
     </div>
+    
   );
 }
 
--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,7 +1,7 @@
 import { faCartShopping } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { useState } from "react"
-import { Dropdown } from "react-bootstrap"
+import { Dropdown, NavDropdown } from "react-bootstrap"
 import Button from "react-bootstrap/Button"
 import Container from "react-bootstrap/Container"
 import Form from "react-bootstrap/Form"
@@ -9,6 +9,9 @@ import Nav from "react-bootstrap/Nav"
 import Navbar from "react-bootstrap/Navbar"
 import Logo from "./Cards/ImagesCards/Logo.png"
 import { Link } from "react-router-dom"
+import { DropdownSubmenu, NavDropdownMenu } from "react-bootstrap-submenu";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './Navegacion.css'
 
 function NavBar() {
     // const { user } = useAuth()
@@ -22,61 +25,60 @@ function NavBar() {
         setIsOpen(false)
     }
     return (
-        <Navbar expand="lg" className="" style={{backgroundColor:"#A6C4C1"}}>
-
+        <Navbar expand="lg" className="" style={{backgroundColor:"#134c8f"}}>
             <Container fluid>
-                <Navbar.Brand className="ms-3" href="/">
-                    Cursos online
-                </Navbar.Brand>
-
-                <Navbar.Brand className='ms-3' href="#">
+                   <Navbar.Brand className='ms-3' href="/">
                     <img src={Logo} className="mx-2" height="50" />
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-
                 <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link
-                            href="#action1"
-                            className="text-wrap"
-                            style={{ fontSize: "14px" }}
-                        >
-                            Categorias
-                        </Nav.Link>
-                    </Nav>
+                    
+                  
+                <NavDropdownMenu title="Categorías" id="collasible-nav-dropdown " className="text-white me-4" >
+                        
+                        <DropdownSubmenu  href="#"  title="Desarrolo Web" className="dropdown-submenu ">
+                            <NavDropdown.Item  href="#" className=""> JavaScript </NavDropdown.Item>
+                            <NavDropdown.Item href="#">React JS</NavDropdown.Item>
+                            <NavDropdown.Item href="#"> Angular</NavDropdown.Item>
+                            <NavDropdown.Item href="#"> CSS </NavDropdown.Item>
+                        </DropdownSubmenu>
+                        <DropdownSubmenu href="#action/3.7" title="Lenguajes de programación" className="dropdown-submenu">
+                            <NavDropdown.Item href="#action/3.2"> Python </NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Java</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.4"> C# </NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.4"> C++ </NavDropdown.Item>
+                        
+                        </DropdownSubmenu>
+                    </NavDropdownMenu>
+                       
 
+                 
+                            
+         
+                    
+                
+               
+               
                     <Form className="d-flex flex-grow-1">
                         <Form.Control
                             type="search"
                             placeholder="Buscar"
-                            className="me-2"
+                            className="me-3"
                             aria-label="Search"
                         />
-                        <Button variant="outline-success">Buscar</Button>
+                        <Button variant="outline-light" className="me-4">Buscar</Button>
+
+                       
+
+                   
+                        
                     </Form>
-
-                    <Nav className="ms-3">
-                        <Nav.Link
-                            href="#action2"
-                            className="text-wrap"
-                            style={{ fontSize: "14px" }}
-                        >
-                            Universae Business
-                        </Nav.Link>
-                        <Nav.Link
-                            href="#action3"
-                            className="text-wrap"
-                            style={{ fontSize: "14px" }}
-                        >
-                            Enseña en Universae
-                        </Nav.Link>
-                    </Nav>
-
+                    
                     <Dropdown show={isOpen} onMouseEnter={handleMouseEnter}>
                         <div>
                             <button
                                 id="carrito"
-                                className="btn btn-outline-success rounded-5 icon__shop"
+                                className="btn btn-outline-light rounded-5 icon__shop me-4"
                             >
                                 <FontAwesomeIcon icon={faCartShopping} />
                             </button>
@@ -97,12 +99,12 @@ function NavBar() {
 
                     <div className="mx-3">
                         <Link to="/login">
-                            <Button className="me-2" variant="outline-success">
+                            <Button className="me-2" variant="outline-light">
                                 Entrar
                             </Button>
                         </Link>
                         <Link to="/register">
-                            <Button variant="outline-success">
+                            <Button variant="outline-light">
                                 Registrate
                             </Button>
                         </Link>
@@ -114,3 +116,25 @@ function NavBar() {
 }
 
 export default NavBar
+
+
+                   {/* <Nav className="ms-3">
+                        <Nav.Link
+                            href="#action2"
+                            className="text-wrap"
+                            style={{ fontSize: "14px" }}
+                        >
+                            Universae Business
+                        </Nav.Link>
+                        <Nav.Link
+                            href="#action3"
+                            className="text-wrap"
+                            style={{ fontSize: "14px" }}
+                        >
+                            Enseña en Universae
+                        </Nav.Link>
+                    </Nav>*/}
+
+                  
+
+                  
