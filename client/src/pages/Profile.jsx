46b7802ab9329490a9f733bee46328eeb26c273f
SitoDP <jesusdprogram@gmail.com>
2024-05-28 14:56:20 +0200

    resolucion problemas de conflicto

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,20 +1,17 @@
 import { Link } from 'react-router-dom'
 import { useContext, useState, useEffect } from 'react'
 import { getCourses } from '../services/courses.service'
-<<<<<<< HEAD
-import { Row, Col } from 'react-bootstrap'; // Asegúrate de importar Row y Col
 import CardUsuario from '../components/Cards/CardUsuario';
-=======
 import { Row, Col, Container } from 'react-bootstrap'; // Asegúrate de importar Row y Col
 import ProfileCard from '../components/Cards/ProfileCard'
 import { AuthContext } from '../context/auth.context'
 import ProfileNav from '../components/Mycourses/ProfileNav';
 
 
->>>>>>> backone
 
 
 const Profile = () => {
+
 
     const { user } = useContext(AuthContext)
     const [courses, setCourses] = useState([])
@@ -41,40 +38,21 @@ const Profile = () => {
             <div>
                 <h1 className='text-white text-center'>Bienvenido {user?.username}</h1>
             </div>
-<<<<<<< HEAD
             <Row className='text-white'>
                 <Col xxl={12}>
                     <h2 className='text-center'>Cursos creados</h2>
-                   <CardUsuario/>
-=======
-
-            <ProfileNav />
-
-
-
-            <Row className='text-white gy-5'>
-                <Col>
-                    <h2 className='text-center'>Todos los cursos</h2>
-                    {courses.length > 0 && courses.map(course => (
-                        <div key={course._id} className='mb-20'>
-                            <h3 className='d-flex justify-content-center'>Título: {course.title}</h3>
-                            <p className='d-flex justify-content-center'>Descripción: {course.description}</p>
-                            <div className='d-flex flex-column text-center column-gap-3 '>
-                                <p>Categoría: {course.category}</p>
-                                <br />
-                                <p>Lenguajes utilizados: <div className='d-flex justify-content-around'>{course.language}</div></p>
-
-                            </div>
-                            <Link to={`/profile/editcourse/${course._id}`}>
-                                <button className='d-flex justify-content-center'>Editar curso</button>
-                            </Link>
-
+                    <ProfileNav />
+                    {userCourses.length > 0 && userCourses.map(course => (
+                        <div key={course._id}>
+                            <CardUsuario course={course} />
                         </div>
                     ))}
+                    <CardUsuario />
 
->>>>>>> backone
+
+
+
                 </Col>
-                
             </Row>
 
 
@@ -82,6 +60,7 @@ const Profile = () => {
 
 
         </>
+
     )
 }
 
