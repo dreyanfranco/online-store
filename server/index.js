082e39a869ed9c8147aaf20965a3f6713fbc33d8
Dreyan Franco <dreyanfranco@gmail.com>
2024-06-10 11:10:55 +0200

    deploy changes

--- server/index.js
+++ server/index.js
@@ -17,8 +17,14 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cors())
 
+app.use(express.static(path.join(__dirname, "../../client/dist")))
+
 require("./routes")(app)
 require("./error-handling")(app)
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../../client/dist/index.html"))
+})
 
 const PORT = process.env.PORT || 5005
 
