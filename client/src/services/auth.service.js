759e1186e28d8e22df48dc11e1aac0a7e09339d3
Dreyan Franco <dreyanfranco@gmail.com>
2024-06-10 12:55:20 +0200

    deploy bug

--- client/src/services/auth.service.js
+++ client/src/services/auth.service.js
@@ -3,7 +3,7 @@ import axios from "axios"
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || ""
 
 const axiosInstance = axios.create({
-    baseURL: `${API_BASE_URL}/auth`,
+    baseURL: `${API_BASE_URL}/api/auth`,
 })
 
 export const register = (userData) => {
