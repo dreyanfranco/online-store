bae45948eff89ce22603c2fbf64493924225791e
Dreyan Franco <dreyanfranco@gmail.com>
2024-06-10 11:16:00 +0200

    added path import

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const cookieParser = require("cookie-parser")
 const cors = require("cors")
+const path = require("path")
 require("dotenv").config()
 require("./db/mongodb")
 const cloudinary = require("cloudinary").v2
